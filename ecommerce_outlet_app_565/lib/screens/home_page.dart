c52509826dcfdd557e77514b6bb8bbf9d3c918ee
phoenix888-y <75326507+phoenix888-y@users.noreply.github.com>
2020-12-03 20:59:56 -0500

    Video 6 part 1
    the video stopped at 7:25

--- ecommerce_outlet_app_565/lib/screens/home_page.dart
+++ ecommerce_outlet_app_565/lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_outlet_app_565/constants.dart';
+import 'package:ecommerce_outlet_app_565/widgets/buttom_tabs.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -11,13 +12,18 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FlatButton(
-          child: Text("Logout"),
-          onPressed: () {
-            FirebaseAuth.instance.signOut();
-          },
-        ),
+      body: Column (
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            child: Center(
+              child: Text(
+                  "Homepage"
+              ),
+            ),
+          ),
+          BottomTabs(),
+        ],
       ),
     );
   }
---
+++ ecommerce_outlet_app_565/lib/widgets/buttom_tabs.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class BottomTabs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: BottomTabButton(),
+    );
+  }
+}
+
+class BottomTabButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image(
+        image: AssetImage(
+          "assets/images/home_tab.png"
+        ),
+      ),
+    );
+  }
+}
